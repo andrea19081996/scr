a8feae1745bc0801caa00a022c83ae85c4ee7e7f
Jerin George Mathew <jerin.edassery95@gmail.com>
2017-05-30 13:38:10 +0200

    Modificate stampe su console (classe Main)

--- SCR-hw/src/it/uniroma3/scr/main/Main.java
+++ SCR-hw/src/it/uniroma3/scr/main/Main.java
@@ -2,7 +2,9 @@ package it.uniroma3.scr.main;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import it.uniroma3.scr.domain.CognitiveTerminal;
 import it.uniroma3.scr.domain.DigitalSignal;
@@ -12,28 +14,46 @@ import it.uniroma3.scr.utilities.UserInterfaceConsole;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException, IOException{
-		int signalLenght=1000000;
+		final int signalLenght=1000000;
+		//definire una costante per il numero di sequenze
+		//definire una costante per il numero di snr
 		//usare una lista costante
 		//sistemare la formattazione del testo stampato sulla console
-		List<Double> snratios=new ArrayList<>();
+		//inserire i javadoc
 		UserInterface io=new UserInterfaceConsole();
-		snratios.add(-3.0);
-		snratios.add(2.0);
-		snratios.add(-8.0);
-		snratios.add(-13.0);
+		List<Double> snratios=new ArrayList<>();
+		Collections.addAll(snratios, -3.0,2.0,-8.0,-13.0);
 		CognitiveTerminal terminal=new CognitiveTerminal(snratios,signalLenght); //definire una variabile per la lunghezza delle sequenze
-
-
+		printSNR2ThresholdValues(io,snratios,terminal);
+		printAllSignalDetectionProbability(3,snratios,terminal,io);
+	}
+	
+	private static void printSNR2ThresholdValues(UserInterface io,List<Double> snratios, CognitiveTerminal terminal){
+		io.println("\tTabella SNR -> Soglia");
+		io.println("\t---------------------");
+		io.println("\tSNR\t\tSoglia");
 		for(Double snr: snratios)
-			io.println("soglia="+terminal.getThreshold(snr)+" snr=" +snr);
-		for(int j=1;j<=3;j++){
-			io.println("Sequenza"+j);
-			for(int i=1;i<=4;i++){
-				io.print("Osservazione"+i+"snr"+snratios.get(i-1)+" ");
-				DigitalSignal signal= new SignalLoader("resources/Sequenze/Sequenza_"+j+"/output_"+i+".dat").loadSignal();
-				io.print(terminal.findPrimaryUser(signal, snratios.get(i-1))+"\t");
-			}
-			io.println("");
+			io.println("\t"+snr+"\t|\t"+terminal.getThreshold(snr));
+		io.println("");
+		io.println("");
+	}
+	private static void printSignalDetectionProbability(int sequenceNumber, List<Double> snratios, CognitiveTerminal terminal,UserInterface io){
+		io.print("\tSequenza "+sequenceNumber);
+		for(int i=1;i<=snratios.size();i++){
+			DigitalSignal signal= new SignalLoader("resources/Sequenze/Sequenza_"+sequenceNumber+"/output_"+i+".dat").loadSignal();
+			double pDetection=terminal.findPrimaryUser(signal, snratios.get(i-1));
+			String truncatedPDetectionValue=new DecimalFormat("#.###").format(pDetection);
+			io.print("\t\t"+snratios.get(i-1)+"dB -> "+truncatedPDetectionValue+"%");
+		}
+		io.println("");
+	}
+	private static void printAllSignalDetectionProbability(int numberOfSequences, List<Double> snratios, CognitiveTerminal terminal, UserInterface io){
+		io.println("\tTabella Segnale -> Probabilita di detection in base a SNR");
+		io.println("\t---------------------------------------------------------");
+
+		for(int j=1;j<=numberOfSequences;j++){
+			printSignalDetectionProbability(j,snratios,terminal,io);
 		}
 	}
+
 }
