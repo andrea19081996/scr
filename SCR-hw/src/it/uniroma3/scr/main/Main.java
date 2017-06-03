e07f120651d48fc2c8e49c751049dfe3ad73b4cc
Jerin George Mathew <jerin.edassery95@gmail.com>
2017-06-03 12:03:51 +0200

    Aggiunte variabili d'istanza alla classe Main

--- SCR-hw/src/it/uniroma3/scr/main/Main.java
+++ SCR-hw/src/it/uniroma3/scr/main/Main.java
@@ -1,7 +1,5 @@
 package it.uniroma3.scr.main;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,46 +11,56 @@ import it.uniroma3.scr.utilities.UserInterface;
 import it.uniroma3.scr.utilities.UserInterfaceConsole;
 
 public class Main {
-	public static void main(String[] args) throws FileNotFoundException, IOException{
-		final int signalLenght=1000000;
+	private final static int SIGNAL_LENGHT=1000000;
+	private UserInterface io;
+	private List<Double> snratios;
+	private CognitiveTerminal terminal; //definire una variabile per la lunghezza delle sequenze
+
+	public Main(){
+		this.io=new UserInterfaceConsole();
+		this.snratios=new ArrayList<>();
+		Collections.addAll(snratios, -3.0,2.0,-8.0,-13.0);
+		this.terminal=new CognitiveTerminal(snratios,SIGNAL_LENGHT);
+	}
+	
+	public static void main(String[] args){
 		//definire una costante per il numero di sequenze
 		//definire una costante per il numero di snr
 		//usare una lista costante
 		//sistemare la formattazione del testo stampato sulla console
 		//inserire i javadoc
-		UserInterface io=new UserInterfaceConsole();
-		List<Double> snratios=new ArrayList<>();
-		Collections.addAll(snratios, -3.0,2.0,-8.0,-13.0);
-		CognitiveTerminal terminal=new CognitiveTerminal(snratios,signalLenght); //definire una variabile per la lunghezza delle sequenze
-		printSNR2ThresholdValues(io,snratios,terminal);
-		printAllSignalDetectionProbability(3,snratios,terminal,io);
+		Main main=new Main();
+		main.printSNR2ThresholdValues();
+		main.printAllSignalDetectionProbability(3);
 	}
 	
-	private static void printSNR2ThresholdValues(UserInterface io,List<Double> snratios, CognitiveTerminal terminal){
-		io.println("\tTabella SNR -> Soglia");
-		io.println("\t---------------------");
-		io.println("\tSNR\t\tSoglia");
-		for(Double snr: snratios)
-			io.println("\t"+snr+"\t|\t"+terminal.getThreshold(snr));
-		io.println("");
-		io.println("");
+	private void printSNR2ThresholdValues(){
+		this.io.println("\tTabella SNR -> Soglia");
+		this.io.println("\t---------------------");
+		this.io.println("\tSNR\t\tSoglia");
+		for(Double snr: this.snratios)
+			this.io.println("\t"+snr+"\t|\t"+this.terminal.getThreshold(snr));
+		this.io.println("");
+		this.io.println("");
 	}
-	private static void printSignalDetectionProbability(int sequenceNumber, List<Double> snratios, CognitiveTerminal terminal,UserInterface io){
-		io.print("\tSequenza "+sequenceNumber);
-		for(int i=1;i<=snratios.size();i++){
+	
+	private void printSignalDetectionProbability(int sequenceNumber){
+		this.io.print("\tSequenza "+sequenceNumber);
+		for(int i=1;i<=this.snratios.size();i++){
 			DigitalSignal signal= new SignalLoader("resources/Sequenze/Sequenza_"+sequenceNumber+"/output_"+i+".dat").loadSignal();
-			double pDetection=terminal.findPrimaryUser(signal, snratios.get(i-1));
+			double pDetection=this.terminal.findPrimaryUser(signal, this.snratios.get(i-1));
 			String truncatedPDetectionValue=new DecimalFormat("#.###").format(pDetection);
-			io.print("\t\t"+snratios.get(i-1)+"dB -> "+truncatedPDetectionValue+"%");
+			this.io.print("\t\t"+this.snratios.get(i-1)+"dB -> "+truncatedPDetectionValue+"%");
 		}
-		io.println("");
+		this.io.println("");
 	}
-	private static void printAllSignalDetectionProbability(int numberOfSequences, List<Double> snratios, CognitiveTerminal terminal, UserInterface io){
-		io.println("\tTabella Segnale -> Probabilita di detection in base a SNR");
-		io.println("\t---------------------------------------------------------");
+	
+	private void printAllSignalDetectionProbability(int numberOfSequences){
+		this.io.println("\tTabella Segnale -> Probabilita di detection in base a SNR");
+		this.io.println("\t---------------------------------------------------------");
 
 		for(int j=1;j<=numberOfSequences;j++){
-			printSignalDetectionProbability(j,snratios,terminal,io);
+			printSignalDetectionProbability(j);
 		}
 	}
 
